c0b50ddf8d01232342c2a4ab956a8014ab10ea4a
reyleal27 <reyvincentleal@gmail.com>
2024-07-14 15:20:59 +0800

    backend model/schema

--- models/index.js
+++ models/index.js
@@ -1,1 +1,11 @@
-const bcrypt = require("bcryptjs");
+const { User } = require("./user");
+const { Product } = require("./product");
+const { DailyNutrition } = require("./dailyNutrition");
+const { Developer } = require("./developers");
+
+module.exports = {
+  User,
+  Product,
+  DailyNutrition,
+  Developer,
+};
--- models/product.js
+++ models/product.js
@@ -1,1 +1,34 @@
-const bcrypt = require("bcryptjs");
+const { Schema, model } = require("mongoose");
+const handleSaveErrors = require("../helpers/handleSaveErrors");
+
+const productSchema = new Schema(
+  {
+    categories: {
+      type: Array,
+      required: [true],
+    },
+    weight: {
+      type: Number,
+      required: [true],
+    },
+    title: {
+      type: Object,
+      required: [true],
+    },
+    calories: {
+      type: Number,
+      required: [true],
+    },
+    groupBloodNotAllowed: {
+      type: Array,
+      required: [true],
+    },
+  },
+  { versionKey: false }
+);
+
+productSchema.post("save", handleSaveErrors);
+
+const Product = model("product", productSchema);
+
+module.exports = { Product };
